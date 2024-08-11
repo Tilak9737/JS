ff2887eebbeb034e3c2a7a6cc1ad108019704482
Tilak Gandhi <118892983+Tilak9737@users.noreply.github.com>
2024-08-11 07:35:03 +0000

    variables of js

---
+++ bsc/var.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let accountEmail = "tilak@google.com"
+var accountPassword = "12345678"
+accountCity = "Vadodara"
+let accountState
+
+// accountId = 2
+accountEmail = "tg@tg.com"
+accountPassword = "1111"
+accountCity = "brd"
+
+
+// console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
